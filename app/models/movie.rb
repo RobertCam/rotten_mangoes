7766b423e9571fc875afbfae9411f01f70ba538e
RobertCam <rob.cameron85@gmail.com>
2015-10-27 23:59:51 +0000

    show page displays average user rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,15 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validate :release_date_is_in_the_past
 
+  def review_average
+    if reviews.size == 0
+      "This movie has no reviews"
+    else
+      "#{reviews.sum(:rating_out_of_ten)/reviews.size}/10"  
+    end
+  end
+
+
   protected
 
   def release_date_is_in_the_past
